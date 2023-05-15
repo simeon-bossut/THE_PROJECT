b424d5397547da6942de3d1d3db2c6721ec58603
Admin <paul-antoine.ngondji@student.junia.com>
2023-05-15 17:07:32 +0200

    sync32434534

--- THE_PROJECT/solver.c
+++ THE_PROJECT/solver.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fill_ghosts(GhostGrid gridf, Grid gridj) {
-	for (int i = 0; i < gridf.size; i++) {
-		for (int j = 0; j < gridf.size; j++) {
-			if (gridj.tab[i][j] != NULL)
-			{
-				;
+void fill_ghost(GhostGrid gridf, Grid gridj, int* around) {
+	if (gridj.tab[0][0] != NULL) {
+		for (int i = 0; i < gridj.size; i++) {
+			for (int j = 0; j < gridj.size; i++) {
+				gridf.tab[i][j] = "1234";
 			}
 		}
+	}
+	else if () {
 	}
 }
 
