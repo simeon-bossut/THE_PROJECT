12f20d27db897f5a59b99cb901f3014c2c8b26ef
SimonBrouxo <simon.broucqsault@student.junia.com>
2023-05-15 16:27:14 +0200

    correction du nav

--- Website_projet/html/nav.php
+++ Website_projet/html/nav.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+
+
+    <link rel="stylesheet" href="../CSS/global.css">
+</head>
+
+
+
 <nav>
   <div id="menucontenu">
 
@@ -10,8 +24,8 @@
     
 
     <ul id="menu">
-      <a href="#"><li>Home</li></a>
-	  <a href="#"><li>Connexion</li></a>
+      <a href="game.php"><li>Home</li></a>
+	    <a href="login.php"><li>Connexion</li></a>
       <a href="#"><li>Game</li></a>
       <a href="#"><li>Contact</li></a>
     </ul>
