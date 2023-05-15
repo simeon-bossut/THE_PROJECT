c73ccb7f8dba0f7dcce6bc5f1d89c4345158f3ac
Admin <paul-antoine.ngondji@student.junia.com>
2023-05-15 18:03:47 +0200

    sync

--- THE_PROJECT/solver.c
+++ THE_PROJECT/solver.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 void fill_ghost(GhostGrid gridf, Grid gridj, int* around) {
+	int row, col,tmp;
 	if (gridj.tab[0][0] != NULL) {
 		for (int i = 0; i < gridj.size; i++) {
 			for (int j = 0; j < gridj.size; i++) {
@@ -11,8 +12,30 @@ void fill_ghost(GhostGrid gridf, Grid gridj, int* around) {
 			}
 		}
 	}
-	else if () {
+	for (int i = 0; i < gridj.size; i++)
+	{
+		row = find_in_grid(gridj, i)->row;
+		col = find_in_grid(gridj, i)->col;
+		for (int j = 0; j < gridj.size; j++)
+		{
+			for (int k = 0; k < gridj.size; k++) {
+				if (k + 1 != i) {
+					gridf.tab[j][col][k] = (char)(k + 1);
+					gridf.tab[row][j][k] = (char)(k + 1);
+					tmp = k;
+				}
+				else {
+					k++;
+					gridf.tab[j][col][k] = (char)(k + 1);
+					gridf.tab[row][j][k] = (char)(k + 1);
+					tmp = k;
+				}
+			}
+			gridf.tab[j][col][tmp] = '\0';
+			gridf.tab[row][j][tmp] = '\0';
+		}
 	}
+
 }
 
 Pos * find_in_grid(Grid grid,int val)
