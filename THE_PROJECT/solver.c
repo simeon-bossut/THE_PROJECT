6651fe7bd8f637117de5a5cc6bbd46f196bbec79
siméon bossut <112804898+simeon-bossut@users.noreply.github.com>
2023-05-15 16:58:44 +0200

    ajout de la fonction find_in _grid

--- THE_PROJECT/solver.c
+++ THE_PROJECT/solver.c
@@ -8,8 +8,28 @@ void fill_ghosts(GhostGrid gridf, Grid gridj) {
 		for (int j = 0; j < gridf.size; j++) {
 			if (gridj.tab[i][j] != NULL)
 			{
-
+				;
 			}
 		}
 	}
 }
+
+Pos * find_in_grid(Grid grid,int val)
+{
+	Pos* positions;
+	int compt = 0;
+	for (int i = 0;i < grid.size;++i)
+	{
+		for (int  j= 0;j < grid.size;++j)
+		{
+			if (grid.tab[i][j] == val)
+			{
+				positions[compt].row = i;
+				positions[compt].col = j;
+			}
+            
+		}
+	}
+	return positions;
+}
+
