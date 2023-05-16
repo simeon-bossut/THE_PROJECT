411ddc96c12fc9ce954176b437587147ac252fd0
Admin <paul-antoine.ngondji@student.junia.com>
2023-05-16 15:57:24 +0200

    sync

--- THE_PROJECT/solver.h
+++ THE_PROJECT/solver.h
@@ -10,5 +10,5 @@ void fill_ghost(GhostGrid gridf, Grid gridj, int* around);
 
 Pos* find_in_grid(Grid grid, int val, int* size);
 
-char* fill_ghost_box();
+char* fill_ghost_box(GhostGrid grid, int value, int i, int j);
 #endif // !solver
