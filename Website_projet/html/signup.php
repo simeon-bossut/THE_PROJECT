fd2044c2b3c39353cae70b0258acfc1a0607b4fa
PaulCauche <112881703+PaulCauche@users.noreply.github.com>
2023-05-16 11:38:20 +0200

    Update signup.php

--- Website_projet/html/signup.php
+++ Website_projet/html/signup.php
@@ -2,10 +2,10 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-	<title>Crapescrapers</title>
+	<title>The Crate Stacker Sign Up</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
+    
     <link rel="stylesheet" href="../CSS/login.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
@@ -22,23 +22,23 @@
 				<?php if (isset($_COOKIE["email_error"])) {echo " <style= text_align: center;> <p> this email address is already in use </p>";
 				setcookie("email_error", "", time() - 3600);} ?>
 				
-				<label>Votre pseudonyme!:</label>
+				<label>Pseudonym :</label>
                 <input class="field_class" name="pseudo" type="text" placeholder="Pseudo" required pattern= "^[A-Za-z ']+$" maxlength="40" required >
 				
-				<label class="input" >Civilité :</label>
-				<input type="radio" name="civility" id="male" value="M" required /><label for="male">Monsieur</label>
-				<input  type="radio" name="civility" id="female" value="Mme" required /><label for="female">Madame</label>
+				<label class="input" >Civility :</label>
+				<input type="radio" name="civility" id="male" value="M" required /><label for="male">Mr</label>
+				<input  type="radio" name="civility" id="female" value="Mme" required /><label for="female">Mrs</label>
 				<br> <br>
 				
-                <label>Password:</label>
+                <label>Password :</label>
                 <input id="pass" class="field_class" name="password" type="password" placeholder="Your password" required >
 				
-				 <label>Confirm your password:</label>
+				 <label>Confirm your password :</label>
                 <input id="pass" class="field_class" name="password2" type="password" placeholder="Your password" required>
-                <button class="bouton" type="submit" name="add">Go!</button>
+                <button class="bouton" type="submit" name="add">Sign Up!</button>
 				<?php if (isset($_COOKIE["password_error"])) {echo " <style= text_align: center;> <p> please choose the same password </p>";
 				setcookie("password_error", "", time() - 3600);} ?>
-				<br><br>
+				
             </div>
 			
         </form>
