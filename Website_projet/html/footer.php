69771ca9f59ff5f75a8173874f5961a82e5bca81
tom-lor-isen <tom.loridant@student.junia.com>
2023-05-16 18:07:28 +0200

    Edit in footer and main page

--- Website_projet/html/footer.php
+++ Website_projet/html/footer.php
@@ -1,32 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport">
-    <meta content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/footer.css">
-</head>
-
-<body>
-    <div class="footer-basic">
-        <footer>
-            <div class="social">
-                <a href="#"><img class="icon ion-social-instagram" src="../logo_reseaux/logo insta p.png"></i></a>
-                <a href="#"><img class="icon ion-social-snapchat" src="../logo_reseaux/logo snap p.png"></i></a>
-                <a href="#"><img class="icon ion-social-twitter" src="../logo_reseaux/logo twitter p.png"></i></a>
-                <a href="#"><img class="icon ion-social-facebook" src="../logo_reseaux/logo facebook p.png"></i></a>
-            </div>
-            <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Home</a></li>
-                <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-            </ul>
-            <p class="copyright">PFD © 2023</p>
-        </footer>
+<link rel="stylesheet" href="../CSS/footer.css">
+<footer>
+    <div class="social">
+        <a href="#"><img class="icon ion-social-instagram" src="../logo_reseaux/logo insta p.png"></a>
+        <a href="#"><img class="icon ion-social-snapchat" src="../logo_reseaux/logo snap p.png"></a>
+        <a href="#"><img class="icon ion-social-twitter" src="../logo_reseaux/logo twitter p.png"></a>
+        <a href="#"><img class="icon ion-social-facebook" src="../logo_reseaux/logo facebook p.png"></a>
     </div>
-</body>
-
-</html>
+    <div class="list-inline">
+        <a href="#">Home</a>
+        <a href="#">Services</a>
+        <a href="#">About</a>
+        <a href="#">Terms</a>
+        <a href="#">Privacy Policy</a>
+    </div>
+    <p class="copyright">PFD © 2023</p>
+</footer>
--- Website_projet/html/game.php
+++ Website_projet/html/game.php
@@ -1,47 +1,90 @@
+<?php session_start(); ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
-	<title>The Crate Staker</title>
+	<title>The Crate Stacker</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="icon" href="./favicon.ico" type="image/x-icon">
 </head>
 
-<body style="overflow: hidden">
+<?php
+  if(!isset($_SESSION['isOnWebsite']) || !$_SESSION['isOnWebsite']) {
+?>
+  <body style="overflow: hidden">
 		
-  <div class="frontDis">
-    <img src="../Images/complete_logo.png" id="crateStackLogo">
-
-    <div id="pressKeyText">Press any key to continue</div>
-  </div>
-
-  <div class="backDis hidden">
-	<?php include("nav.php"); ?>
-
-  <main>
-    <h1>Welcome to The Crate Stacker game !</h1>
-		<br> <br> <br>
-		<h2>The Crate Stacker is similar to Skyscrapers, which is a puzzle game that involves placing buildings on a square grid with some clues on the sides.</h2>
-    <br>
-    <div id="part1">
-      <p>The goal of Skyscrapers is to place a skyscraper in each square, with a height between 1 and N, so that no two skyscrapers in the same row or column have the same number of floors.</p>
-      <br>
+    <div class="frontDis">
+      <img src="../Images/complete_logo.png" id="crateStackLogo">
+  
+      <div id="pressKeyText">Press any key to continue</div>
     </div>
-    <div id="part2">
-      <p>Note that the tallest skyscrapers block the view of the lower skyscrapers behind them. The men on the hill must see the number of buildings that are marked in front of them.</p>
+  
+    <div class="backDis hidden">
+    
+  <?php include("nav.php"); ?>
+  
+    <main>
+      <h1>Welcome to The Crate Stacker game !</h1>
+      <br> <br> <br>
+      <h2>The Crate Stacker is similar to Skyscrapers, which is a puzzle game that involves placing buildings on a square grid with some clues on the sides.</h2>
       <br>
-    </div>
-    <div id="part3">
-      <p>In Crate Stacker, we don't stack skyscraper floors but crates stored in a warehouse, the goal remains the same: stack our crates while seeing the number of crates on the sides of our grid.</p>
-    </div>  
-  </main>
+      <div id="part1">
+        <p>The goal of Skyscrapers is to place a skyscraper in each square, with a height between 1 and N, so that no two skyscrapers in the same row or column have the same number of floors.</p>
+        <br>
+      </div>
+      <div id="part2">
+        <p>Note that the tallest skyscrapers block the view of the lower skyscrapers behind them. The men on the hill must see the number of buildings that are marked in front of them.</p>
+        <br>
+      </div>
+      <div id="part3">
+        <p>In Crate Stacker, we don't stack skyscraper floors but crates stored in a warehouse, the goal remains the same: stack our crates while seeing the number of crates on the sides of our grid.</p>
+      </div>  
+    </main>
 
-	<?php include("footer.php"); ?>
+<?php include("footer.php"); ?>
 
-</div>
-	
+</body>
   <script src="script.js"></script>
+  
+
+<?php
+    $_SESSION['isOnWebsite'] = true;
+  }
+
+  else { 
+
+    echo '<body>';
+    include("nav.php");
+
+?>
+
+<main>
+      <h1>Welcome to The Crate Stacker game !</h1>
+      <br> <br> <br>
+      <h2>The Crate Stacker is similar to Skyscrapers, which is a puzzle game that involves placing buildings on a square grid with some clues on the sides.</h2>
+      <br>
+      <div id="part1">
+        <p>The goal of Skyscrapers is to place a skyscraper in each square, with a height between 1 and N, so that no two skyscrapers in the same row or column have the same number of floors.</p>
+        <br>
+      </div>
+      <div id="part2">
+        <p>Note that the tallest skyscrapers block the view of the lower skyscrapers behind them. The men on the hill must see the number of buildings that are marked in front of them.</p>
+        <br>
+      </div>
+      <div id="part3">
+        <p>In Crate Stacker, we don't stack skyscraper floors but crates stored in a warehouse, the goal remains the same: stack our crates while seeing the number of crates on the sides of our grid.</p>
+      </div>  
+    </main>
+
+  <?php include("footer.php"); ?>
   </body>
+
+  <?php
+} 
+
+?>
 </html>
