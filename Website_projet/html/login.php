a2e3ef13940c20f7c22b028138ad9c0676a14955
PaulCauche <112881703+PaulCauche@users.noreply.github.com>
2023-05-16 13:40:53 +0200

    Update login.php

--- Website_projet/html/login.php
+++ Website_projet/html/login.php
@@ -1,30 +1,66 @@
+<?php 
+session_start();
+$find = true;
+try {
+	if (isset($_POST["con"])) {
+		//echo "ça marche omg";
+		require("connexion.php");
+		    $login = $_POST["login"];
+			$password = $_POST["password"];
+			$reqPrep = "SELECT * FROM acc"; //La requere SQL: SELECT
+			$req = $conn->prepare($reqPrep); //Préparer la requete
+			$req->execute(); //Executer la requete
+
+			$resultat = $req->fetchALl(PDO::FETCH_ASSOC); //récupération du résultat 
+			foreach ($resultat as $row) {
+				if ($row["password"] == $password && $row["email"] == $login){
+					        $_SESSION["authentifie"] = true;
+							$_SESSION["email"] = $row["email"];
+							$_SESSION["civility"] = $row["civility"];
+							setcookie("email", $row["email"], time() + 60 * 60 * 24 * 30);
+							header("Location: game.php");
+									}
+			
+			else{
+				$find = false;
+			}
+			}
+	}
+}catch (Exception $e) {
+die("Erreur : " . $e->getMessage());}
+	
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-	<title>Crate Stacker connexion</title>
+	<title>Crapescrapers</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
     <link rel="stylesheet" href="../CSS/login.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
-
   <body>
-
   <?php include("nav.php"); ?>
-
     <header>
         <h1>login portal</h1>
     </header>
-
     <main>
-        <form class="form" action="" method=" post">
+        <form class="form" action="" method="post">
             <div class="form_div">
                 <label>Login:</label>
-                <input class="field_class" name="login_txt" type="text" placeholder="Your Email" autofocus>
+                <input class="field_class" name="login" type="text" placeholder="Your Email" autofocus>
                 <label>Password:</label>
-                <input id="pass" class="field_class" name="password_txt" type="password" placeholder="Your password">
-                <button class="bouton" type="submit" form="login_form">Go!</button>
+                <input id="pass" class="field_class" name="password" type="password" placeholder="Your password">
+                <button class="bouton" type="submit" name="con">Go!</button>
+				          <?php if (!$find) {
+            echo "mot de passe ou login erroné";
+          } ?>
             </div>
             <div class="newacc">
                 <p>you don't have an account yet? <a href="signup.php">Sign up!</a></p>
