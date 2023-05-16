8f568a851f58f37b64df1a30e51d10e07e4e0369
SimonBrouxo <simon.broucqsault@student.junia.com>
2023-05-16 11:43:50 +0200

    ta mère la mise en page

--- Website_projet/html/signup.php
+++ Website_projet/html/signup.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8">
 	<title>The Crate Stacker Sign Up</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
     <link rel="stylesheet" href="../CSS/login.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
-  </head>
-  <body>
+</head>
+
+<body>
+
   <?php include("nav.php"); ?>
+
     <header>
         <h1>Sign up portal</h1>
     </header>
+
     <main>
         <form class="form" action="newacc.php" method="post">
             <div class="form_div">
                 <label>Email (login):</label>
                 <input class="field_class" name="email" type="text" placeholder="Your Email"  maxlength="40" required >
+
 				<?php if (isset($_COOKIE["email_error"])) {echo " <style= text_align: center;> <p> this email address is already in use </p>";
 				setcookie("email_error", "", time() - 3600);} ?>
 				
@@ -33,18 +38,19 @@
                 <label>Password :</label>
                 <input id="pass" class="field_class" name="password" type="password" placeholder="Your password" required >
 				
-				 <label>Confirm your password :</label>
+				<label>Confirm your password :</label>
                 <input id="pass" class="field_class" name="password2" type="password" placeholder="Your password" required>
                 <button class="bouton" type="submit" name="add">Sign Up!</button>
+
 				<?php if (isset($_COOKIE["password_error"])) {echo " <style= text_align: center;> <p> please choose the same password </p>";
 				setcookie("password_error", "", time() - 3600);} ?>
 				
             </div>
-			
         </form>
     </main>
 	
 		<?php include("footer.php"); ?>
 	
 </body>
+
 </html>
