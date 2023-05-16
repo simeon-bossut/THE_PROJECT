a0af1d2af468a1ee5e2ad12cf831665b1999863b
Admin <paul-antoine.ngondji@student.junia.com>
2023-05-16 15:47:35 +0200

    sync fill_ghost

--- THE_PROJECT/solver.c
+++ THE_PROJECT/solver.c
@@ -5,7 +5,8 @@
 
 void fill_ghost(GhostGrid gridf, Grid gridj, int* around) {
 	int tmp; int l = 0;
-	int* cols; int* rows; // Pos of searched numbers
+	int* cols; // Pos of searched numbers
+	int* rows;
 	Pos* pos = (Pos*)malloc(gridj.size*sizeof(Pos)); // Pos storage
 	if (gridj.tab[0][0] != 0) { // Filling ghost grid with all possibilities if it's empty
 		for (int i = 0; i < gridj.size; i++) {
@@ -15,16 +16,22 @@ void fill_ghost(GhostGrid gridf, Grid gridj, int* around) {
 		}
 	}
 	int size;
-	for (int i = 0; i < gridj.size; i++)
+	for (int i = 0; i < gridf.size; i++)
 	{
 		pos = find_in_grid(gridj, i,&size);
-		cols = (int*)malloc(sizeof(int) * size); rows = (int*)malloc(sizeof(int) * size);
+		cols = (int*)malloc(sizeof(int) * size);
+		rows = (int*)malloc(sizeof(int) * size);
 		for (int j = 0; j < size; j++)
 		{
 			rows[j] = pos[j].row;
 			cols[j] = pos[j].col;
 		}
-
+		for (int j = 0; j < gridf.size; j++) {
+			for (int k = 0; k < gridf.size; k++) {
+				fill_ghost_box(gridf, i, rows[j], k);
+				fill_ghost_box(gridf, i, k, cols[j]);
+			}
+		}
 	}
 
 }
@@ -54,7 +61,7 @@ Pos * find_in_grid(Grid grid,int val,int*size)//attention grid.size diff de size
 }
 
 char* fill_ghost_box(GhostGrid grid,int value, int i, int j) {
-	int tmp;
+	int tmp = 0;
 	for (int k = 0; k < grid.size; k++)
 	{
 		if (k != value) {
@@ -67,4 +74,5 @@ char* fill_ghost_box(GhostGrid grid,int value, int i, int j) {
 		}
 		tmp++;
 	}
+	grid.tab[i][j][tmp] = '\0';
 }
