ead22e33fed351732c7479768e8e84c0caca4029
siméon bossut <112804898+simeon-bossut@users.noreply.github.com>
2023-05-17 16:28:03 +0200

    line_to_id works

--- THE_PROJECT/main.c
+++ THE_PROJECT/main.c
@@ -7,30 +7,44 @@
 int main(int argc, char **argv) {
   srand(time(NULL));
 
-  int size = argv[1] ? atoi(argv[1]) : 3;
+  //int size = argv[1] ? atoi(argv[1]) : 3;
 
-  int *pov;
-  pov = initpov(size);
+  //int *pov;
+  //pov = initpov(size);
 
-  Grid *grid = initgrid(size);
-  GhostGrid* gridf = initGhostGrid(size);
-  if (grid == NULL) {
-    return 0;
-  }
+  //Grid *grid = initgrid(size);
+  //GhostGrid* gridf = initGhostGrid(size);
+  //if (grid == NULL) {
+  //  return 0;
+  //}
 
-  // fillgrid(grid);
-  //  grid_completion(grid);
+  //// fillgrid(grid);
+  ////  grid_completion(grid);
 
-  printgrid(grid, pov);
+  //printgrid(grid, pov);
 
 
 	//test seed
-	/*int val = 15;
+	int val = 15;
 	char id[3] = "7";
-	char *line;
-	line = id_to_line(id);
+	//char *line;
 
-	printf("%s ", line);*/
+	//line = id_to_line(id,5);
+	char line[6] = "43521\0";
+
+	int ID = line_to_id(line, 5);
+
+	char *line_2;
+		
+	line_2=id_to_line(ID,5);
+
+	printf("la ligne est %s \n", line);
+
+	printf("l'ID associé est %d\n", ID);
+
+	printf("la ligne retrouvée est %s", line_2);
+
+
 	
 
   return EXIT_SUCCESS;
--- THE_PROJECT/seed.h
+++ THE_PROJECT/seed.h
@@ -3,8 +3,9 @@
 
 #include "game.h"
 
-char* id_to_line(char*Id);
+char* id_to_line(int val,int dim);
 
-char* line_to_id(char*line);
+int line_to_id(char*line,int dim);
 
+int factorial(int n);
 #endif // !seed
