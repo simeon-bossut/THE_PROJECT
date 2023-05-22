017c0ad6184e646a99f0635b4d6db144868e6ba1
YanisOuchene <yanis.ouchene@student.junia.com>
2023-05-22 14:46:21 +0200

    e

--- THE_PROJECT/main.c
+++ THE_PROJECT/main.c
@@ -20,18 +20,18 @@ int main(int argc, char **argv) {
   //  grid_completion(grid);
 
   grid->tab[0][0] = 3;
-  grid->tab[1][0] = 2;
-  grid->tab[2][0] = 4;
-  grid->tab[3][0] = 1;
-  grid->tab[0][1] = 0;
+  grid->tab[1][0] = 0;
+  grid->tab[2][0] = 0;
+  grid->tab[3][0] = 0;
+  grid->tab[0][1] = 2;
   grid->tab[1][1] = 0;
   grid->tab[2][1] = 0;
   grid->tab[3][1] = 0;
-  grid->tab[0][2] = 0;
+  grid->tab[0][2] = 4;
   grid->tab[1][2] = 0;
   grid->tab[2][2] = 0;
   grid->tab[3][2] = 0;
-  grid->tab[0][3] = 0;
+  grid->tab[0][3] = 1;
   grid->tab[1][3] = 0;
   grid->tab[2][3] = 0;
   grid->tab[3][3] = 0;
