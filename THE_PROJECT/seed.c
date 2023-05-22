52e20a21d3d320718f4fddf3b13ff22ab5612fe0
YanisOuchene <yanis.ouchene@student.junia.com>
2023-05-22 14:25:54 +0200

    sync

--- THE_PROJECT/seed.c
+++ THE_PROJECT/seed.c
@@ -1,30 +1,23 @@
 #include "seed.h"
 #include "game.h"
-#include "solver.h"
-
 int factorial(int n) {
   if (n == 0)
     return 1;
   else
     return n * factorial(n - 1);
 }
-
 int line_to_id(char *line, int dim) {
   // Il faut idéalement mettre un char ne contenant que des chiffres
   // différent(ligne d'un carré latin)
   int val = 0; // Ce char va être transformé à la fin en char grace to itoa
-
   int factor = factorial(dim - 1);
-
   val += ((line[0] - 48) - 1) * factor +
          1; // Le 6 vient du fait de 3! possibilités une fois le char[0] fixé
   if (dim == 3) {
     if (line[1] > line[2]) {
       val += 1;
     }
-  } else if (dim == 4)
-
-  {
+  } else if (dim == 4) {
     if (line[1] > line[2]) // C des trucs que j'ai trouvé, demandez si vous
                            // voulez comprendre
     {
@@ -46,21 +39,17 @@ int line_to_id(char *line, int dim) {
         compt++;
       }
     }
-
     for (int i = 0; i < dim - 1; ++i) {
       if (tab[i] == line[1]) {
         val += i * 6;
       }
     }
-
     if (line[3] > line[4]) {
       val += 1;
     }
-
     if (line[2] > line[3]) {
       val += 2;
     }
-
     if (line[2] > line[4]) {
       val += 2;
     }
@@ -69,14 +58,12 @@ int line_to_id(char *line, int dim) {
   char Id[4] = {0}; // 3 chiffres max + \0
   return val;
 }
-
 char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
 {                                  // Parti pour la galère
   val = val % (factorial(dim));
   if (val == 0) {
     val = factorial(dim);
   }
-
   char *line = malloc(
       sizeof(char) * (dim + 1)); // pour l'intant 4 (+\0)car taille de la ligne,
                                  // par le futur malloc car taille variable
@@ -86,28 +73,22 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
   for (int i = 0; i < dim + 1; ++i) {
     line[i] = 0;
   }
-
   line[0] = ((val - 1) / factorial(dim - 1) + 1) +
             48; // N'hésitez pas à poser des questions
-
   char *tab = malloc(
       sizeof(char) *
       (dim)); // tableau contenant tous les chiffres sauf celui dans line[0]
   if (tab == NULL) {
     return NULL;
   }
-
   tab[dim - 1] = 0;
-
   int compt = 0;
-
   for (int i = 0; i < dim; ++i) {
     if ((i + 1) + 48 != line[0]) {
       tab[compt] = (i + 1) + 48;
       compt++;
     }
   }
-
   if (dim == 3) {
     for (int i = 0; i < dim - 1; ++i) {
       if (line[2 - val % 2] == 0) // Si cette case déja remplie déja remplie
@@ -119,16 +100,13 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
     }
     return line;
   }
-
   if (dim == 4) {
     line[1] =
         tab[((val - 1) % 6 / 2)]; // Chiant à expliquer en commentaires(étude de
                                   // la ligne de longueur 6)
   }
-
   if (dim == 5) {
     line[1] = tab[((val - 1) % 24 / 6)];
-
     for (int i = ((val - 1) % 24 / 6); i < dim - 1;
          i++) // suppresion de line[1] dans tab et décalage adapté
     {
@@ -136,7 +114,6 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
     }
     tab[dim - 2] = '\0';
   }
-
   if (dim == 4) {
     for (int i = 0; i < dim - 1; ++i) {
       if (tab[i] != line[1]) // Si le chiffre est encore dispo
@@ -150,10 +127,8 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
       }
     }
   }
-
   if (dim == 5) {
     line[2] = tab[(val - 1) % 6 / 2];
-
     for (int i = 0; i < dim - 1; ++i) {
       if (tab[i] != line[2]) // Si le chiffre est encore dispo
       {
@@ -168,7 +143,6 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
   }
   return line;
 }
-
 // char *create_seed(int difficulty, int dim) {
 //   int size;
 //   if (dim == 3) {
@@ -191,7 +165,6 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
 //   {
 //     ;
 //   }
-
 //   int size_cache = dim * (dim + 4);
 //   bool *cache = malloc(sizeof(bool) * size_cache);
 //   if (cache == NULL) {
@@ -205,7 +178,6 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
 //     }
 //   } else {
 //     do {
-
 //     } while (solver()); // Tant que le solveur marche
 //   }
 //   int value = 0b0;
@@ -217,7 +189,6 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
 //       value, SEED + 1, dim * dim,
 //       10); // Dans le futur à décaler de quelques cases car le tableau
 //       précède
-
 //   value = 0b0;
 //   for (int i = dim * dim; i < size_cache; ++i) {
 //     value += cache[i];
@@ -228,21 +199,16 @@ char *id_to_line(int val, int dim) // Uniquement en 4*4 pour l'instant
 //       10); // DAns le futur à décaler de quelques cases car le tableau
 //       précède
 // }
-
 int *Dec2Bin(int n, int dim) {
   int *binaryNum = malloc(sizeof(int) * dim * dim);
-
   int i = 0;
   while (n > 0) {
-
     binaryNum[i] = n % 2;
     n = n / 2;
     i++;
   }
-
   return binaryNum;
 }
-
 int *get_cache_tab(int dim, char *Seed, int len) {
   int *cache_tab = malloc(sizeof(int) * dim * dim);
   if (cache_tab == NULL) {
@@ -252,91 +218,20 @@ int *get_cache_tab(int dim, char *Seed, int len) {
   memcpy(tmp_cache_tab, Seed + dim + 1, dim);
 
   int int_cache_tab = atoi(tmp_cache_tab);
-
   cache_tab = Dec2Bin(int_cache_tab, dim);
-
   return cache_tab;
 }
+int *get_cache_obv(int dim, char *Seed, int len) {
+  int *cache_tab = malloc(sizeof(int) * dim * dim * 4);
+  if (cache_tab == NULL) {
+    return NULL;
+  }
+  char *tmp_cache_tab = malloc(sizeof(char) * dim * dim * 4);
+  memcpy(tmp_cache_tab, Seed + dim * 2 + 1, dim + 1);
 
-	int size_cache = dim * (dim + 4);
-	bool* cache = malloc(sizeof(bool) *size_cache);
-	if (cache == NULL)
-	{
-		return NULL;
-	}
-	if (difficulty == 1)
-	{
-		for (int i = 0;i < size_cache;++i)
-		{
-			cache[i] = i/(dim*dim);//Tableau facile , tous les observateurs sont visibles et tout le jeu caché
-		}
-	}
-	else
-	{
-		do {
-            ;
-		} while (1);//Tant que le solveur marche, encore à coder
-	}
-	int value = 0b0;
-	for (int i = 0;i <dim*dim ;++i)
-	{
-		value += cache[i];
-		value = value << 1;
-	}
-	_itoa_s(value, SEED + 1, dim * dim, 10);//Dans le futur à décaler de quelques cases car le tableau précède
-
+  int int_cache_tab = atoi(tmp_cache_tab);
+  cache_tab = Dec2Bin(int_cache_tab, dim * 4);
   return cache_tab;
-}
-
-}
-
-void getLeftCases(char*tab,int i, int j,Grid*grid,int size)
-{
-    int compt = 0;
-    for (int i = 0;i < size;++i)
-    {
-        if (!found_in_col(i + 1, grid, j) && !found_in_row(i + 1, grid, i))
-        {
-            tab[compt] = i + 1;
-            compt++;
-        }
-    }
-    tab[compt] = '\0';
-}
-
-
-
-void generateGrid(Grid*grid,char* leftCases) {
-
-    int compt = 0;
-    int size = grid->size;
-    for (int i = 0; i < size; i++)
-    {
-        leftCases[i] = i + 1;
-    }
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-
-            getLeftCases(leftCases,i,j,grid,size);
-
-            if (strlen(leftCases) == 0) {
-                compt++;
-                if (compt > 3000) {
-                    compt = 0;
-                    return;//error
-                }
-
-                generateGrid(grid,leftCases);
-            }
-
-            grid->tab[i][j];
-        }
-    }
-
-    compt = 0;
-
-    return;
 }
 Grid *read_seed_3dim(Grid *grid, int dim, char *Seed, int len) {
   int *cache_tab;
@@ -349,49 +244,26 @@ Grid *read_seed_3dim(Grid *grid, int dim, char *Seed, int len) {
       grid->tab[i][j] = line[j] - 48;
     }
   }
-
   cache_tab = get_cache_tab(dim, Seed, len);
-
   for (int i = 0; i < dim; i++) {
     for (int j = 0; j < dim; j++) {
       if (cache_tab[i + j] == 0)
         grid->tab[i][j] = 0;
     }
   }
-
-
-
-
-
-
-
-
-
-
-Grid *read_seed_3dim(Grid *grid, int dim, char *Seed, int len) {
-  for (int i = 1; i < len; i++) {
-    if (i < 4) {
-      for (int j = 0; j < dim; j++) {
-        char *line = id_to_line(Seed[i], dim);
-        grid->tab[i - 1][j] = line[0] - 48;
-      }
   cache_obv = get_cache_obv(dim, Seed, len);
-
   for (int i = 0; i < dim * 4; i++) {
     if (cache_obv[i] == 0)
       grid->obv[i] = 0;
   }
-
   return grid;
 }
 Grid *read_seed_4dim(Grid *grid, int dim, char *Seed, int len) { return grid; }
 Grid *read_seed_5dim(Grid *grid, int dim, char *Seed, int len) { return grid; }
-
 Grid *read_seed(char *Seed) {
   int lenSeed = strlen(Seed);
   int dim = Seed[0] - 48;
   Grid *grid = initgrid(dim);
-
   switch (dim) {
   case 3:
     grid = read_seed_3dim(grid, dim, Seed, lenSeed);
@@ -403,6 +275,5 @@ Grid *read_seed(char *Seed) {
     grid = read_seed_5dim(grid, dim, Seed, lenSeed);
     break;
   }
-
   return grid;
 }
