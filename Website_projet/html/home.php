1a6b7fff49099d0889caadcdeafeff980cd11a7e
tom-lor-isen <tom.loridant@student.junia.com>
2023-05-22 15:06:58 +0200

    Home Remade

--- Website_projet/html/home.php
+++ Website_projet/html/home.php
@@ -34,25 +34,55 @@
       echo '<div class="backDis">';
     } ?>
 
-      <?php include("nav.php"); ?>
+    <nav>
+      <div class="buttonsLogo">
+        <a href="conceptor.php"><div class="buttonLogo">Level Creator</div></a>
+        <a href="settings.php"><div class="buttonLogo">Settings</div></a>
+      </div>
+      <a href=""><img src="../Images/complete_logo.png" id="logo"></a>
+      <div class="buttonsLogo">
+        <a href="ranking.php"><div class="buttonLogo">Leaderboard</div></a>
+        <a href="login.php"><div class="buttonLogo">Login / Sign up</div></a>
+      </div>
+    </nav>
 
     <main>
-      <h1>The Crate Stacker game !</h1>
-      <h2>The Crate Stacker is similar to Skyscrapers, which is a puzzle game that involves placing buildings on a square grid with some clues on the sides.</h2>
-      <br>
-      <div id="part1">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/Ww3HbHMnJ5g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  
-        <p>The goal of Skyscrapers is to place a skyscraper in each square, with a height between 1 and N, so that no two skyscrapers in the same row or column have the same number of floors.</p>
-        <br>
+
+      <div class="gameTemplate">
+
+        <canvas id="mainPlate"></canvas>
+
+        <canvas id="secondPlate"></canvas>
+
       </div>
-      <div id="part2">
-        <p>Note that the tallest skyscrapers block the view of the lower skyscrapers behind them. The men on the hill must see the number of buildings that are marked in front of them.</p>
-        <img width="560" height="315" src="../images/skyscraper_stage.png">
-        <br>
+
+      
+      
+      <div class="creamBG">
+        <h2 class="titleContainer">What is it ?</h2>
+        <p>The Crate Stacker is similar to Skyscrapers, which is a puzzle game that involves placing buildings on a square grid with some clues on the sides.</p>
       </div>
-      <div id="part3">
+      
+      <div class="brownBG">
+        <h2 class="titleContainer">The Goal</h2>
+
+        <div class="containerContent">
+          <iframe src="https://www.youtube.com/embed/Ww3HbHMnJ5g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  
+          <p>The goal of Skyscrapers is to place a skyscraper in each square, with a height between 1 and N, so that no two skyscrapers in the same row or column have the same number of floors.</p>
+        </div>
+
+        <div class="containerContent">
+          <p>Note that the tallest skyscrapers block the view of the lower skyscrapers behind them. The men on the hill must see the number of buildings that are marked in front of them.</p>
+          <img src="../images/skyscraper_stage.png">
+        </div>
+
+      </div>
+
+      <div class="creamBG">
+        <h2 class="titleContainer">Our Game</h2>
         <p>In Crate Stacker, we don't stack skyscraper floors but crates stored in a warehouse, the goal remains the same: stack our crates while seeing the number of crates on the sides of our grid.</p>
-      </div>  
+      </div>
+
     </main>
     
     <?php if(isset($_SESSION['isOnWebsite']) && $_SESSION['isOnWebsite']) { include("footer.php"); } ?>
