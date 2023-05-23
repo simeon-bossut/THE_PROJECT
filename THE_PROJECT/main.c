660d008578bee0c7f36ad161a2d237718eae74d3
siméon bossut <112804898+simeon-bossut@users.noreply.github.com>
2023-05-23 15:58:03 +0200

    sync

--- THE_PROJECT/main.c
+++ THE_PROJECT/main.c
@@ -54,32 +54,39 @@ int main(int argc, char **argv) {
    free(grid); free(gridf);
    printf("\n");*/
 
-  Grid* grid = initgrid(5);
-  if(grid==NULL)
+  //Grid* grid = initgrid(5);
+  //if(grid==NULL)
+  //{
+	 // return NULL;
+  //}
+  //generateGrid(grid);
+  //for(int i = 0;i < grid->size;++i)
+  //{
+	 // for (int j = 0;j < grid->size;++j)
+	 // {
+		//  printf("%d |",grid->tab[i][j]);
+	 // }
+	 // printf("\n");
+  //}
+  //
+
+  //char *Seed3dim = "32635114095";
+  //char *Seed4dim = "4012418066553565535";
+  //char *Seed5dim = "5001120060112099335544311048575";
+  //grid = read_seed(Seed3dim);
+  //printgrid(grid);
+
+  //grid = read_seed(Seed4dim);
+  //printgrid(grid);
+
+  //grid = read_seed(Seed5dim);
+  //printgrid(grid);
+  int value = 34627;
+  char* buffer = malloc(sizeof(char) * 6);
+  if (buffer == NULL)
   {
 	  return NULL;
   }
-  generateGrid(grid);
-  for(int i = 0;i < grid->size;++i)
-  {
-	  for (int j = 0;j < grid->size;++j)
-	  {
-		  printf("%d |",grid->tab[i][j]);
-	  }
-	  printf("\n");
-  }
-  
-
-  char *Seed3dim = "32635114095";
-  char *Seed4dim = "4012418066553565535";
-  char *Seed5dim = "5001120060112099335544311048575";
-  grid = read_seed(Seed3dim);
-  printgrid(grid);
-
-  grid = read_seed(Seed4dim);
-  printgrid(grid);
-
-  grid = read_seed(Seed5dim);
-  printgrid(grid);
+  intoa(value, buffer, 5);
   return EXIT_SUCCESS;
 }
