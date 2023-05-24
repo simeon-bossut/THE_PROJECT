225b756659a767ee5d3f579996627cea41edc7af
tom-lor-isen <tom.loridant@student.junia.com>
2023-05-24 16:45:26 +0200

    Coucou

--- Website_projet/html/game.js
+++ Website_projet/html/game.js
@@ -7,6 +7,13 @@ function insertElement(set, element) {
 
 }
 
+function getElemByCoord(x, y) {
+
+  var element = document.querySelector(`#pos_${x}_${y}`);
+
+  return element;
+}
+
 
 function initMainPlate() {
 
@@ -25,43 +32,41 @@ function initMainPlate() {
 
     for(let x = -1; x < tabDim * 2 + 1 + 3; x++) {
 
-      console.log(x % 2 == 0, y);
-
       if(x == 0 && y == 0)
-        insertElement(gameSet, `<div class="cornerRoad rotate00"></div>`);
+        insertElement(gameSet, `<div class="cornerRoad rotate00" id="pos_${x}_${y}"></div>`);
 
       else if(x == tabDim * 2 && y == 0)
-        insertElement(gameSet, `<div class="cornerRoad rotate90"></div>`);
+        insertElement(gameSet, `<div class="cornerRoad rotate90" id="pos_${x}_${y}"></div>`);
 
       else if(x == 0 && y == tabDim * 2)
-        insertElement(gameSet, `<div class="cornerRoad rotate270"></div>`);
+        insertElement(gameSet, `<div class="cornerRoad rotate270" id="pos_${x}_${y}"></div>`);
 
       else if(x == tabDim * 2 && y == tabDim * 2 && y < tabDim * 2 + 1)
-        insertElement(gameSet, `<div class="cornerRoad rotate180"></div>`);
+        insertElement(gameSet, `<div class="cornerRoad rotate180" id="pos_${x}_${y}"></div>`);
 
       else if(x % 2 == 0 && y % 2 == 1 && x <= tabDim * 2 && y < tabDim * 2 + 1)
-        insertElement(gameSet, `<div class="straightRoad rotate00"></div>`);
+        insertElement(gameSet, `<div class="straightRoad rotate00" id="pos_${x}_${y}"></div>`);
 
       else if(x % 2 == 1 && y % 2 == 0 && x <= tabDim * 2)
-        insertElement(gameSet, `<div class="straightRoad rotate90"></div>`);
+        insertElement(gameSet, `<div class="straightRoad rotate90" id="pos_${x}_${y}"></div>`);
 
       else if(x == 0 && y % 2 == 0 && x <= tabDim * 2)
-        insertElement(gameSet, `<div class="TRoad rotate00"></div>`);
+        insertElement(gameSet, `<div class="TRoad rotate00" id="pos_${x}_${y}"></div>`);
 
       else if(x == tabDim * 2 && y % 2 == 0 && x <= tabDim * 2)
-        insertElement(gameSet, `<div class="TRoad rotate180"></div>`);
+        insertElement(gameSet, `<div class="TRoad rotate180" id="pos_${x}_${y}"></div>`);
 
       else if(x % 2 == 0 && y == 0 && x <= tabDim * 2)
-        insertElement(gameSet, `<div class="TRoad rotate90"></div>`);
+        insertElement(gameSet, `<div class="TRoad rotate90" id="pos_${x}_${y}"></div>`);
       
       else if(x % 2 == 0 && y == tabDim * 2 && x <= tabDim * 2)
-        insertElement(gameSet, `<div class="TRoad rotate270"></div>`);
+        insertElement(gameSet, `<div class="TRoad rotate270" id="pos_${x}_${y}"></div>`);
       
       else if(x % 2 == 0 && y % 2 == 0 && x > 0 && x < tabDim * 2 && y > 0 && y < tabDim * 2)
-        insertElement(gameSet, `<div class="crossRoad rotate00"></div>`);
+        insertElement(gameSet, `<div class="crossRoad rotate00" id="pos_${x}_${y}"></div>`);
       
       else
-        insertElement(gameSet, `<div class="emptyYet"></div>`)
+        insertElement(gameSet, `<div id="pos_${x}_${y}"></div>`)
 
     }
 
