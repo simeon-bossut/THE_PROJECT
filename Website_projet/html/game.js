84822878f89a7d797ebd2e7f4ee9677af3d8a034
tom-lor-isen <tom.loridant@student.junia.com>
2023-05-26 11:27:24 +0200

    Finished Gameplay

--- Website_projet/html/game.js
+++ Website_projet/html/game.js
@@ -10,8 +10,8 @@ var obsTab = [];
 var crateTab = [];
 
 for(let i = 0; i < tabDim * 4; i++) {
-  obsTab.push(i);
-  crateTab.push(i);
+  obsTab.push(Math.floor(Math.random() * (tabDim + 1)));
+  crateTab.push(Math.floor(Math.random() * (tabDim + 1)));
 }
 
 
@@ -28,7 +28,8 @@ class Character {
 
     this.direction = "W";
 
-    console.log("---", this.x, this.y, "---")
+    this.numCrate = 0;
+
 
     document.querySelector(`#pos_${this.x}_${this.y}`).classList.toggle("hasPlayer");
     document.querySelector(`#pos_${this.x}_${this.y}`).innerHTML = `<div id="characterBox" class="rotate270">
@@ -131,8 +132,11 @@ function insertElement(set, x, y, classList) {
   else if (type.contains("CStraight") || type.contains("CCorner"))
     name = "boxPick";
   
-  else if (type.contains("crateArea"))
-    name = "crateArea";
+  else if (type.contains("cratePlace"))
+    name = "cratePlace";
+  
+  else if (type.contains("Trash"))
+    name = "trash";
   
   else name = "empty";
 
@@ -179,8 +183,6 @@ function initMainPlate() {
 
     for(let x = -1; x < tabDim * 2 + 2 + 1; x++) {
 
-      console.log(x, y);
-
       // Corner Road
       if(x == 0 && y == 0)
         insertElement(gameSet, x, y, `cornerRoad rotate00`);
@@ -218,21 +220,23 @@ function initMainPlate() {
       else if (x % 2 == 0 && y % 2 == 0 && x > 0 && x < tabDim * 2 && y > 0 && y < tabDim * 2)
         insertElement(gameSet, x, y, `crossRoad rotate00`);
     
-    // Observators
-    else if((x == -1) && y % 2 == 1 && y > 0 && y <= tabDim * 2)
-      insertElement(gameSet, x, y, `obs rotate270`);
+      // Observators
+      else if((x == -1) && y % 2 == 1 && y > 0 && y <= tabDim * 2)
+        insertElement(gameSet, x, y, `obs rotate270`);
 
-    else if(y == -1 && x % 2 == 1 && x > 0 && x <= tabDim * 2)
-      insertElement(gameSet, x, y, `obs rotate00`);
-    
-    else if(x == tabDim * 2 + 1 && y % 2 == 1 && y > 0 && y <= tabDim * 2)
-      insertElement(gameSet, x, y, `obs rotate90`);
+      else if(y == -1 && x % 2 == 1 && x > 0 && x <= tabDim * 2)
+        insertElement(gameSet, x, y, `obs rotate00`);
+      
+      else if(x == tabDim * 2 + 1 && y % 2 == 1 && y > 0 && y <= tabDim * 2)
+        insertElement(gameSet, x, y, `obs rotate90`);
 
-    else if(y == tabDim * 2 + 1 && x % 2 == 1 && x > 0 && x <= tabDim * 2)
-      insertElement(gameSet, x, y, `obs rotate180`);
-    
+      else if(y == tabDim * 2 + 1 && x % 2 == 1 && x > 0 && x <= tabDim * 2)
+        insertElement(gameSet, x, y, `obs rotate180`);
+      
+      else if(x > 0 && x < tabDim * 2 && y > 0 && y < tabDim * 2 && x % 2 == 1 && y % 2 == 1)
+        insertElement(gameSet, x, y, `cratePlace rotate00`);
       else
-        insertElement(gameSet, x, y, `empty rotate00`);
+          insertElement(gameSet, x, y, `empty rotate00`);
 
     }
 
@@ -296,19 +300,115 @@ function initMainPlate() {
     insertElement(gameSet, 11, 6, "CCorner rotate90");
   }
 
-  document.querySelectorAll(".obs").forEach((element, index) => {
-    let rotParent = Number(element.classList[1].split("tate")[1]);
-    let rotation = (- rotParent) % 360 == 0 ? "00" : String((720 - rotParent) % 360);
 
-    element.innerHTML += `<div class="obsText rotate${rotation}">${obsTab[index]}</div>`;
+  // OBSERVATORS DISPLAYING
+  var count = 0;
 
-    if(index % tabDim * 2 + 2 + 2) {
-      //element.innerHTML = obsTab[index];
-      console.log(index);
-    }
+  for(let i = 0; i < tabDim; i++) {
+    let element = document.querySelector(`#pos_${i * 2 + 1}_-1`);
+    element.innerHTML += `<div class="obsText rotate00">${obsTab[count]}</div>`;
+
+    count++;
+  }
+
+  for(let i = 0; i < tabDim; i++) {
+    let element = document.querySelector(`#pos_${tabDim * 2 + 1}_${i * 2 + 1}`);
+    element.innerHTML += `<div class="obsText rotate270">${obsTab[count]}</div>`;
+
+    count++;
+  }
+
+  for(let i = tabDim - 1; i >= 0; i--) {
+    let element = document.querySelector(`#pos_${i * 2 + 1}_${tabDim * 2 + 1}`);
+    element.innerHTML += `<div class="obsText rotate180">${obsTab[count]}</div>`;
+
+    count++;
+  }
+
+  for(let i = tabDim - 1; i >= 0; i--) {
+    let element = document.querySelector(`#pos_-1_${i * 2 + 1}`);
+    element.innerHTML += `<div class="obsText rotate90">${obsTab[count]}</div>`;
+
+    count++;
+  }
+
+
+  // CRATES DISPLAYING
+
+  document.querySelectorAll(".cratePlace").forEach((element, index) => {
+    if(crateTab[index] != 0)
+      element.innerHTML = crateTab[index];
   })
 
+
 }
+
+
+function crateAction() {
+  let xAdd = 0;
+  let yAdd = 0;
+
+  if(player.direction == "E")
+    xAdd++;
+  else if(player.direction == "W")
+    xAdd--;
+  else if(player.direction == "N")
+    yAdd--;
+  else if(player.direction == "S")
+    yAdd++;
+
+  if(gameTab[player.x + 1 + xAdd][player.y + 1 + yAdd] == "cratePlace") {
+    let crateArea = document.querySelector(`#pos_${player.x + xAdd}_${player.y + yAdd}`);
+
+    let qty =  Number(crateArea.textContent);
+
+
+    // Pick a box
+    if(player.numCrate == 0 && qty > 0) {
+      player.numCrate++;
+      qty--;
+    }
+
+    // Place a box on stack
+    else if(player.numCrate > 0 && qty < tabDim) {
+      player.numCrate--;
+      qty++;
+    }
+
+    if(qty == 0) qty = "";
+
+    crateArea.textContent = qty;
+  }
+
+  // Facing a pick area
+  else if(gameTab[player.x + 1 + xAdd][player.y + 1 + yAdd] == "boxPick") {
+
+    if(player.numCrate == tabDim)
+      return;
+    
+    player.numCrate++;  
+  }
+
+  // Facing a trash
+  else if(gameTab[player.x + 1 + xAdd][player.y + 1 + yAdd] == "trash") {
+
+    if(player.numCrate > 0)
+      player.numCrate--;
+  }
+
+
+  // Draw Player Crate number
+  let num = player.numCrate;
+
+  if(num == 0)
+    num = "";
+    
+  document.querySelector("#characterCrate").innerHTML = num;
+
+}
+
+
+
 
 const listKeys = ['KeyW', 'KeyA', 'KeyS', 'KeyD', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight', 'Space', 'Enter'];
 
@@ -322,22 +422,31 @@ window.addEventListener('keydown', e => {
 document.addEventListener('keydown', e => {
   let key = e.code;
 
-    if (key == "KeyA" || key == "ArrowLeft") {
-      player.move("W");
-    }
-    else if (key == "KeyD" || key == "ArrowRight") {
-      player.move("E");
-    }
-    else if (key == "KeyS" || key == "ArrowDown") {
-      player.move("S");
-    }
-    else if (key == "KeyW" || key == "ArrowUp") {
-      player.move("N");
-    }
-    else if (key == "KeyP") {
-      setView();
-    }
-  })
+  // Movements Player
+  if (key == "KeyA" || key == "ArrowLeft") {
+    player.move("W");
+  }
+  else if (key == "KeyD" || key == "ArrowRight") {
+    player.move("E");
+  }
+  else if (key == "KeyS" || key == "ArrowDown") {
+    player.move("S");
+  }
+  else if (key == "KeyW" || key == "ArrowUp") {
+    player.move("N");
+  }
+
+  // POV Player
+  else if (key == "KeyP") {
+    setView();
+  }
+
+  // Crates actions
+  else if(key == "Space") {
+    crateAction();
+  }
+  
+})
 
   
 initMainPlate();
