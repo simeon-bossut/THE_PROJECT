9ba9aba05cdbfd1233d03bc2b6a1045b54e60812
tom-lor-isen <tom.loridant@student.junia.com>
2023-05-26 18:02:19 +0200

    Big Edits in settings

--- Website_projet/html/setting.php
+++ Website_projet/html/setting.php
@@ -36,75 +36,80 @@ if (isset($_POST['checked'])) {
 			<h1>Custom your game </h1>
 		</header>
 
-		<div class="choice"> 
-			<form class="oui" action="" method="post">
-				<h1>Choose your language</h1>
-		<label class="lablelg">English
-			<input type="radio" checked="checked" name="checked"  value="uk">
-			<img src="../Images/ukflag.png" class="flag" alt="French flag" > 
-		</label>
-		<label class="lablelg">French
-
-			<input type="radio"  name="checked" value="fr">
-		<img src="../Images/France.png" class="flag" alt="French flag" > 
-		</label>
-			<button class="bouton" type="submit" name="Modifier">Choose!</button>
-		</form>
-		</div>	
-
-		<div class="form">
-			<h1 class="settingh1"> Modify your account data </h1>
-
-			<form action="edit.php" method="post">
-				<div class="form_div">
-
-					<label>Email</label>
-					<input class="field_class" name="email" type="text" placeholder="<?php echo $_SESSION["email"]; ?>" required autofocus>
-
-					<?php if (isset($_COOKIE["email_edit_error"])) {
-						echo '<div style="font-size:14px;color:red;text-align:center"> <p>This email is already taken</p> </div>';
-						setcookie("email_edit_error", "", time() - 3600);
-					} ?>
-
-					<button class="bouton" type="submit" name="edit_mail">Modifier</button>
-				</div>
-			</form>
-
-			<form action="edit.php" method="post">				
-				<div class="form_div">
-					<label>Pseudo</label>
-					<input class="field_class" name="pseudo" type="text" placeholder=<?php echo $_SESSION["pseudo"]; ?> required>
-
-					<button class="bouton" type="submit" name="edit_pseudo">Modifier</button>
-				</div>
-			</form>
-
-			<form action="edit.php" method="post">
-				<div class="form_div">
-					<label>New Password</label>
-					<input id="pass" class="field_class" name="newPassword1" type="password" placeholder="New password" required>
-					<label>Confirm your new Password :</label>
-					<input id="pass" class="field_class" name="newPassword2" type="password" placeholder="New password" required>
-
-					<?php if (isset($_COOKIE["password_edit_error2"])) {
-						echo '<div style="font-size:14px;color:red;text-align:center"> <p>Please choose the same password</p> </div>';
-						setcookie("password_edit_error2", "", time() - 3600);
-					} ?>
+		<div class="container">
+			<div class="choice"> 
+			<h2>Choose your language</h2>
+				<form action="" method="post">
+					<div class="formInputs">
+						<div class="lablelg">
+							<input type="radio" checked="checked" name="checked"  value="uk">
+							<img src="../Images/ukflag.png" class="flag" alt="French flag" >
+							English
+						</div>
+						<div class="lablelg">
+							<input type="radio"  name="checked" value="fr">
+							<img src="../Images/France.png" class="flag" alt="French flag" >
+							Français
+						</div>
+					</div>
 					
-					
-					<label>Confirm old password</label>
-					<input id="pass" class="field_class" name="oldPassword" type="password" placeholder="Your password" required>
+					<button class="bouton" type="submit" name="Modifier">Choose!</button>
+				</form>
+			</div>	
 
-					<?php if (isset($_COOKIE["password_edit_error"])) {
-						echo '<div style="font-size:14px;color:red;text-align:center"> <p>Please enter the right password</p> </div>';
-						setcookie("password_edit_error", "", time() - 3600);
-					} ?>
+			<div class="form">
+				<h2>Modify your account</h2>
 
-					<button class="bouton" type="submit" name="edit_pass">Modifier</button>
-				</div>
-			</form>
+				<form action="edit.php" method="post">
+					<div class="form_div">
+
+						<label>Email</label>
+						<input class="field_class" name="email" type="text" placeholder="<?php echo $_SESSION["email"]; ?>" required autofocus>
+
+						<?php if (isset($_COOKIE["email_edit_error"])) {
+							echo '<div style="font-size:14px;color:red;text-align:center"> <p>This email is already taken</p> </div>';
+							setcookie("email_edit_error", "", time() - 3600);
+						} ?>
+
+						<button class="bouton" type="submit" name="edit_mail">Change</button>
+					</div>
+				</form>
+
+				<form action="edit.php" method="post">				
+					<div class="form_div">
+						<label>Pseudo</label>
+						<input class="field_class" name="pseudo" type="text" placeholder=<?php echo $_SESSION["pseudo"]; ?> required>
+
+						<button class="bouton" type="submit" name="edit_pseudo">Change</button>
+					</div>
+				</form>
+
+				<form action="edit.php" method="post">
+					<div class="form_div">
+						<label>New Password</label>
+						<input id="pass" class="field_class" name="newPassword1" type="password" placeholder="New password" required>
+						<label>Confirm your new Password :</label>
+						<input id="pass" class="field_class" name="newPassword2" type="password" placeholder="New password" required>
+
+						<?php if (isset($_COOKIE["password_edit_error2"])) {
+							echo '<div style="font-size:14px;color:red;text-align:center"> <p>Please choose the same password</p> </div>';
+							setcookie("password_edit_error2", "", time() - 3600);
+						} ?>
+						
+						
+						<label>Confirm old password</label>
+						<input id="pass" class="field_class" name="oldPassword" type="password" placeholder="Your password" required>
+
+						<?php if (isset($_COOKIE["password_edit_error"])) {
+							echo '<div style="font-size:14px;color:red;text-align:center"> <p>Please enter the right password</p> </div>';
+							setcookie("password_edit_error", "", time() - 3600);
+						} ?>
+
+						<button class="bouton" type="submit" name="edit_pass">Change</button>
+					</div>
+				</form>
+			</div>
 		</div>
-
 	</main>
 
 	<?php include("footer.php"); ?>
