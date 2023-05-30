c188eaa2c8decde918a5a73866ae6632d609762b
PaulCauche <112881703+PaulCauche@users.noreply.github.com>
2023-05-30 12:06:45 +0200

    Update language.php

--- Website_projet/html/language.php
+++ Website_projet/html/language.php
@@ -3,7 +3,6 @@ function getLanguage($textFr, $textEn) {
   if (isset($_COOKIE["lang"]) && $_COOKIE["lang"] == 'fr')
     return $textFr;
   else {
-    setcookie("lang", 'en', time() +(60*60*24*30*365));
     return $textEn;
   }
 }
