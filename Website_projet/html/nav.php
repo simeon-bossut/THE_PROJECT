ec25e878635230dfedcd34cb6842fe58162ff5d1
PaulCauche <112881703+PaulCauche@users.noreply.github.com>
2023-05-30 10:04:14 +0200

    lang for nav.php

--- Website_projet/html/nav.php
+++ Website_projet/html/nav.php
@@ -1,3 +1,5 @@
+<?php include'language.php'; ?>
+
 <nav>
 
   <div id="menucontenu">
@@ -12,21 +14,21 @@
 
     <ul id="menu">
 
-      <a href="home.php"><li>Home</li></a>
+      <a href="home.php"><li><?php  echo getLanguage("Accueil","Home");  ?></li></a>
 	    
       <?php if(isset($_SESSION["authentifie"]) &&  $_SESSION["authentifie"] == true) { ?>
 	    
-        <a href="logout.php"><li>Log Out</li></a>
+        <a href="logout.php"><li><?php  echo getLanguage("Déconnexion","Logout");  ?></li></a>
 	    
       <?php } else { ?>
 	 
-        <a href="login.php"><li>Log In</li></a>
+        <a href="login.php"><li><?php  echo getLanguage("Connexion","Login");  ?></li></a>
 	    
       <?php } ?>
 
-      <a href="conceptor.php"><li>Level Conceptor</li></a>
-      <a href="ranking.php"><li>Ranking</li></a>
-      <a href="setting.php"><li>Settings</li></a>
+      <a href="conceptor.php"><li><?php  echo getLanguage("Créateur de niveau","Level Conceptor");  ?></li></a>
+      <a href="ranking.php"><li><?php  echo getLanguage("Classement","Ranking");  ?></li></a>
+      <a href="setting.php"><li><?php  echo getLanguage("Paramètres","Settings");  ?></li></a>
     </ul>
   </div>
 </nav>
