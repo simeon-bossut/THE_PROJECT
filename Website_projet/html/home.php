7617daf8baa630ca72b0b6626dab2387c11c3e8a
tom-lor-isen <tom.loridant@student.junia.com>
2023-05-30 09:47:41 +0200

    Corrected Glitches

--- Website_projet/html/home.php
+++ Website_projet/html/home.php
@@ -23,7 +23,7 @@
     <div class="frontDis">
       <img src="../Images/complete_logo.png" id="crateStackLogo">
 
-      <div id="pressKeyText">Click or press to continue</div>
+      <div id="pressKeyText">Click to continue</div>
     </div>
 
     <script src="script.js" defer></script>
