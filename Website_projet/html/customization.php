3ee536bda0d6f3c4d5b89943af460fe4c1fcdc8e
Paul Cauche <paul.cauche@student.junia.com>
2023-06-01 10:43:19 +0200

    cookie hat

--- Website_projet/html/customization.php
+++ Website_projet/html/customization.php
@@ -131,7 +131,8 @@
 	
 	function changeHat() {
 		let src = hatSelector.options[hatSelector.selectedIndex].getAttribute("src");
-
+		let id =hatSelector.options[hatSelector.selectedIndex].getAttribute("class");
+		document.cookie = `hat=${id};60*60*24*30`;
 		document.querySelector(".hat").style.backgroundImage = `url("${src}")`;
 	}
 
